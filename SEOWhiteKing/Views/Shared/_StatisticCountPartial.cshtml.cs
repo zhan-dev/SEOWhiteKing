ea8339e069b9cfa6034c1c922f34a057d6c74de3
Zhan <Zhan.Kariahin@gmail.com>
2025-06-29 21:20:29 +0300

    гзвфеу

---
+++ SEOWhiteKing/Views/Shared/_StatisticCountPartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SEOWhiteKing.Views.Shared
+{
+    public class _StatisticCountPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
